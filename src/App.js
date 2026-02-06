d9cfb0c108b235b27d3e9d79eda3c78a913dc225
Chaeng-Ploypaphat-S <ploypaphat.saltz@gmail.com>
2026-02-05 21:00:07 -0800

    [refactor]: experiment using class version instead

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import Input from './Input';
 import './App.css';
 
@@ -9,6 +9,11 @@ function HellWorld(props) {
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
     const [dob, setDob] = useState("")
+    
+    // refs
+    const firstNameRef = useRef();
+    const lastNameRef = useRef(null); // forward ref use 'null'
+    const dobRef = useRef(null);
 
     const toggleTrue = () => {
         if (isTrue) {
@@ -18,27 +23,66 @@ function HellWorld(props) {
         setIsTrue(true)
     }
 
+    // This is a Hook that allows component's side effect
     useEffect(() => {
         console.log("useEffect")
         let people = [
             {
                 id: 1,
-                name: "Bell",
-                lastname: "ok",
+                firstName: "Bell",
+                lastName: "aok",
+                dob: "1999-02-01",
             },
             {
                 id: 2,
-                name: "Jack",
-                lastname: "oktoo",
+                firstName: "Jack",
+                lastName: "zoktoo",
+                dob: "1999-02-02",
             },
             {
                 id: 3,
-                name: "Cola",
-                lastname: "okagain",
+                firstName: "Cola",
+                lastName: "bokagain",
+                dob: "1999-02-03",
             },
         ]
         setCrowd(people)
     }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (lastName !== "") {
+            addPerson(firstName, lastName, dob)
+        }
+    }
+
+    const addPerson = (newFirst, newLast, newDOB) => {
+        let newPerson = {
+            id: crowd.length + 1,
+            firstName: newFirst,
+            lastName: newLast,
+            dob: newDOB,
+        }
+
+        const newList = crowd.concat(newPerson)
+        const sortedCrowd = newList.sort((a, b) => {
+            if (a.lastName < b.lastName) {
+                return -1;
+            } else if (a.lastName > b.lastName) {
+                return 1;
+            }
+            return 0;
+        })
+
+        setCrowd(sortedCrowd);
+        setFirstName("")
+        setLastName("")
+        setDob("")
+
+        firstNameRef.current.value = "";
+        lastNameRef.current.value = "";
+        dobRef.current.value = "";
+    }
 
     return (
         <>
@@ -57,8 +101,9 @@ function HellWorld(props) {
         : <p className='p-orange'>Is False</p>
         }
         <a href="#!" className='btn btn-outline-secondary' onClick={toggleTrue}>Toggle IsTrue</a>
+        
         <hr />
-        <form autoComplete='off'>
+        <form autoComplete='off' onSubmit={handleSubmit}>
 
             <div className='mb-3'>
                 <label className='form-label' htmlFor="first-name">
@@ -67,7 +112,8 @@ function HellWorld(props) {
                 <input 
                 type="text" 
                 name="first-name" 
-                id="first-name" 
+                id="first-name"
+                ref={firstNameRef}
                 autoComplete='first-name-new' 
                 className='form-control'
                 onChange={(event) => setFirstName(event.target.value)}
@@ -78,6 +124,7 @@ function HellWorld(props) {
                 title="Last Name"
                 type="text"
                 name="last-name"
+                ref={lastNameRef}
                 autoComplete="last-name-new"
                 className="form-control"
                 onChange={(event) => setLastName(event.target.value)}
@@ -87,10 +134,13 @@ function HellWorld(props) {
                 title="DOB"
                 type="date"
                 name="dob"
+                ref={dobRef}
                 autoComplete="dob-new"
                 className="form-control"
                 onChange={(event) => setDob(event.target.value)}
             ></Input>
+
+            <input type="submit" value="Submit" className='btn btn-primary'></input>
 
         </form>
         <div>
@@ -102,7 +152,9 @@ function HellWorld(props) {
         <h3>People</h3>
         <ul className='list-group'>
             {crowd.map((m) => (
-                <li key={m.id} className='list-group-item'>{m.name}</li>
+                <li key={m.id} className='list-group-item'>
+                    {m.firstName} {m.lastName} {m.dob}
+                </li>
             ))}
         </ul>
         <hr />
--- src/AppClass.js
+++ src/AppClass.js
@@ -1,12 +1,47 @@
 import React, {Component, Fragment} from 'react';
 import './AppClass.css';
+import Input from './Input';
 
 export default class AppClass extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+
+        this.firstNameRef = React.createRef();
+        this.lastNameRef = React.createRef(null);
+        this.dobRef = React.createRef(null);
+
         this.state = {
             isTrue: false,
+            crowd: [],
         };
+    }
+
+    componentDidMount() {
+        this.setState({
+            firstName: "",
+            lastName: "",
+            dob: "",
+            crowd: [
+            {
+                id: 1,
+                firstName: "BellClass",
+                lastName: "aok",
+                dob: "1999-02-01",
+            },
+            {
+                id: 2,
+                firstName: "JackClass",
+                lastName: "zoktoo",
+                dob: "1999-02-02",
+            },
+            {
+                id: 3,
+                firstName: "ColaClass",
+                lastName: "bokagain",
+                dob: "1999-02-03",
+            },
+            ]
+        })
     }
 
     toggleTrue = () => {
@@ -19,6 +54,44 @@ export default class AppClass extends Component {
         this.setState({
             isTrue: true
         })
+    }
+
+    setFirstName(newName) {
+        this.setState({firstName: newName})
+    }
+
+    addPerson(newFirst, newLast, newDOB) {
+        let newPerson = {
+            id: this.state.crowd.length + 1,
+            firstName: newFirst,
+            lastName: newLast,
+            dob: newDOB,
+        }
+
+        const newList = this.state.crowd.concat(newPerson)
+        const sortedCrowd = newList.sort((a, b) => {
+            if (a.lastName < b.lastName) {
+                return -1;
+            } else if (a.lastName > b.lastName) {
+                return 1;
+            }
+            return 0;
+        })
+
+        this.setState({crowd: sortedCrowd})
+        this.setState({firstName: "", lastName: "", dob: ""})
+        
+        this.firstNameRef.current.value = "";
+        this.lastNameRef.current.value = "";
+        this.dobRef.current.value = "";
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (this.state.firstName !== "") {
+            this.addPerson(this.state.firstName, this.state.lastName, this.state.dob)
+        }
     }
 
     render (){
@@ -38,7 +111,65 @@ export default class AppClass extends Component {
             ? <p>Is True</p>
             : <p className='p-orange'> Is False</p>
             }
-            <a href="#!" className='btn btn-outline-secondary' onClick={this.toggleTrue}>Toggle IsTrue</a>
+            <a href="#!" className='btn btn-outline-secondary' onClick={this.toggleTrue}>
+                Toggle IsTrue</a>
+            
+            <hr />
+            <form autoComplete='off' onSubmit={this.handleSubmit}>
+
+                <div className='mb-3'>
+                    <label className='form-label' htmlFor="first-name">
+                        First Name
+                    </label>
+                    <input 
+                    type="text" 
+                    name="first-name" 
+                    id="first-name"
+                    ref={this.firstNameRef}
+                    autoComplete='first-name-new' 
+                    className='form-control'
+                    onChange={(event) => this.setFirstName(event.target.value)}
+                    ></input>
+                </div>
+
+                <Input
+                    title="Last Name"
+                    type="text"
+                    name="last-name"
+                    ref={this.lastNameRef}
+                    autoComplete="last-name-new"
+                    className="form-control"
+                    onChange={(event) => this.setState({lastName: event.target.value})}
+                ></Input>
+
+                <Input
+                    title="DOB"
+                    type="date"
+                    name="dob"
+                    ref={this.dobRef}
+                    autoComplete="dob-new"
+                    className="form-control"
+                    onChange={(event) => this.setState({dob: event.target.value})}
+                ></Input>
+
+                <input type="submit" value="Submit" className='btn btn-primary'></input>
+            </form>
+
+            <div>
+                First Name: {this.state.firstName} <br />
+                Last Name: {this.state.lastName} <br />
+                DOB: {this.state.dob} <br />
+            </div>
+            <hr />
+            <h3>People</h3>
+            <ul className='list-group'>
+                {this.state.crowd.map((m) => (
+                    <li key={m.id} className='list-group-item'>
+                        {m.firstName} {m.lastName} {m.dob}
+                    </li>
+                ))}
+            </ul>
+            <hr />
             </>
         );
     }
